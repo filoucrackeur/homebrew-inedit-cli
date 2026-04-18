c4b5ff6aa0c2e5cb5303f10c967615c45193101c
Philippe <philippe@MacBook-Air-de-Marina.local>
2026-04-18 14:56:17 +0200

    Use prebuilt binaries instead of building from source

--- Formula/inedit-cli.rb
+++ Formula/inedit-cli.rb
@@ -1,19 +1,28 @@
 class IneditCli < Formula
   desc "A terminal-based INI/conf file editor with interactive TUI"
   homepage "https://github.com/filoucrackeur/inedit-cli"
-  url "https://github.com/filoucrackeur/inedit-cli/archive/refs/tags/v0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "e306d977df81fcac598974698eb8f18086d65f14e1c470a5e836c4dd29343ede"
   license "MIT"
 
-  depends_on "rust" => :build
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/filoucrackeur/inedit-cli/releases/download/v0.1.0/inedit-cli-macos-arm64.tar.gz"
+      sha256 "59a48fbf889e30c6dab4f1f6b40d4a7bcc3eefa4ababd16500be725f44fb9cc2"
+    else
+      url "https://github.com/filoucrackeur/inedit-cli/releases/download/v0.1.0/inedit-cli-macos-x64.tar.gz"
+      sha256 "d7dfb72a1924ca26223f9c5876248204c62085467bfc1484521f6996b37e9f33"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/filoucrackeur/inedit-cli/releases/download/v0.1.0/inedit-cli-linux-x64.tar.gz"
+    sha256 "2bee0e1bf25456fe3628596162e50293b19ab3c43dc7612533eb633e63a1c1e5"
+  end
 
   def install
-    system "cargo", "build", "--release", "--locked"
-    bin.install "target/release/inedit-cli"
+    bin.install "inedit-cli"
   end
 
   test do
-    assert_match "^inedit-cli", shell_output("#{bin}/inedit-cli --version 2>&1", 1)
+    assert_match "inedit-cli", shell_output("#{bin}/inedit-cli --version 2>&1", 1)
   end
 end
